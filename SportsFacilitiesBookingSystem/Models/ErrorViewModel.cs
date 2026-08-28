a22833b3e38122f96fcd2d87956bc2cf6be46f28
Enuri Senara Uyanage <enuriuyanage16@gmail.com>
2026-08-28 21:08:08 +0530

    Add initial ASP.NET Core MVC scaffold

---
+++ SportsFacilitiesBookingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SportsFacilitiesBookingSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
