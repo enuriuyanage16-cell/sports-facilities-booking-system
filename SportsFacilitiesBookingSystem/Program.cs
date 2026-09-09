efbc811620decaf7143e269022ff87dadde88bac
Enuri Senara Uyanage <enuriuyanage16@gmail.com>
2026-09-09 20:01:35 +0530

    Register DbContext in Program.cs

--- SportsFacilitiesBookingSystem/Program.cs
+++ SportsFacilitiesBookingSystem/Program.cs
@@ -1,7 +1,13 @@
+using SportsFacilitiesBookingSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<SportsBookingDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SportsBookingDBConnection")));
 
 var app = builder.Build();
 
